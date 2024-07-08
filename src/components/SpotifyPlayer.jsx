84c0a7a52c665fc26b66de7827440feb9711084a
Ariel Lagos <ariellagoslira@gmail.com>
2024-07-08 14:39:24 -0400

    2° commit

---
+++ src/components/SpotifyPlayer.jsx
@@ -0,0 +1,44 @@
+/**
+ * Playlist component
+ */
+export const SpotifyPlayer = ({url, favorite}) => {
+  const urlSong = "https://open.spotify.com/embed/album/" + url;
+  const favoriteSong = favorite;
+  /**
+   * Render que se ejecuta si el album es favorito
+   */
+  if (favoriteSong) {
+    return (
+      <div className="d-flex my-5" width="1000px">
+        <iframe
+          src={urlSong}
+          width="100%"
+          height="80"
+          allow="autoplay; clipboard-write;
+    encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+        <button className="btn">
+          <i class="bi bi-star-fill"></i>
+        </button>
+      </div>
+    );
+  } else {
+  /**
+   * Render que se ejecuta si el album no es favorito
+   */
+    return (
+      <div className="d-flex my-5" width="1000px">
+        <iframe
+          src={urlSong}
+          width="100%"
+          height="80"
+          allow="autoplay; clipboard-write;
+    encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+        <button className="btn">
+          <i className="bi bi-star"></i>
+        </button>
+      </div>
+    );
+  }
+};
